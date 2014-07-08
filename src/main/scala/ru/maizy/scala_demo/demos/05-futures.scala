7b32cb92f9779e66f9b58545eec459726ee80b85
Nikita Kovaliov <nikita@maizy.ru>
2014-07-08 20:21:15 +0400

    futures & partial functions

--- src/main/scala/ru/maizy/scala_demo/demos/05-futures.scala
+++ src/main/scala/ru/maizy/scala_demo/demos/05-futures.scala
@@ -3,7 +3,8 @@ package ru.maizy.scala_demo.demos
  * Copyright (c) Nikita Kovaliov, maizy.ru, 2014
  * See LICENSE.txt for details.
  */
-import scala.concurrent.{future, Future}
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration._
 
 import ru.maizy.scala_demo.{Settings, Demo}
 import ru.maizy.scala_demo.demoBlock
@@ -17,24 +18,26 @@ class FuturesDemo extends Demo {
     class MyError(msg: String) extends Exception(msg)
     class MyError2(msg: String) extends Exception(msg)
 
-    def universalCallbackBuilder(label: String): PartialFunction[Any, Unit] =
+    def sleep(sec: Double): Unit = {Thread.sleep((sec * 1000).toInt)}
+
+    def universalCallbackBuilder(label: String = ""): PartialFunction[Any, Unit] =
       {
-        case i: Int if i >= 1 => println(s"$label: OK >= 1: $i")
-        case e: MyError => println(s"$label: $label: My error: $e")
+        case i: Int if i >= 1 => println(s"universal callback, $label: OK >= 1: $i")
+        case e: MyError => println(s"$label: My error catched at universal callback: $e")
       }
 
-    val universalCallback: PartialFunction[Any, Unit] = universalCallbackBuilder("")
+    val universalCallback: PartialFunction[Any, Unit] = universalCallbackBuilder()
 
-    val myErrorMatchers: PartialFunction[Exception, Unit] = {
-      case e: MyError => println(s"My error: $e")
-    }
+    def myErrorCallbackBuilder(label: String): PartialFunction[Throwable, Unit] = {
+        case e: MyError => println(s"$label: My error catched: $e")
+      }
 
     val intWithConditions: PartialFunction[Int, Unit] = {
       case i if i >= 4 => println(s"Int: >=4 - $i")
     }
 
     val alwaysMatchCallback: PartialFunction[Any, Unit] = {
-      case other => println(s"Ooops: $other")
+      case other => println(s"Ooops catch something unknown: $other")
     }
 
     demoBlock("partial functions") {
@@ -53,36 +56,101 @@ class FuturesDemo extends Demo {
     demoBlock("simple future callbacks") {
 
       val simpleFuture: Future[Int] = Future {
-        println("simpleFuture compute"); 5
+        println("simpleFuture compute")
+        sleep(sec = 0.2)
+        println("simpleFuture finished")
+        5
       }
+
+      simpleFuture onSuccess universalCallbackBuilder("simpleFuture #1")
+      Await.result(simpleFuture, 2 seconds)
 
       val otherFuture: Future[Int] = Future {
-        println("otherFuture compute"); 3
+        println("otherFuture compute")
+        sleep(sec = 0.2)
+        println("otherFuture finished")
+        3
       }
 
-      simpleFuture onSuccess universalCallbackBuilder("simpleFuture")
-      simpleFuture onSuccess universalCallbackBuilder("simpleFuture #2")
-      simpleFuture onFailure universalCallbackBuilder("simpleFuture onFailure")
+      otherFuture onSuccess universalCallbackBuilder("otherFuture #1") //multiple callbacks allowed
+      otherFuture onSuccess universalCallbackBuilder("otherFuture #2") //callback order not garanted
+      otherFuture onSuccess universalCallbackBuilder("otherFuture #3")
 
-      otherFuture onSuccess universalCallbackBuilder("otherFuture")
-      otherFuture onFailure universalCallbackBuilder("otherFuture onFailure")
+      Await.result(otherFuture, 2 seconds)
 
+      Future {
+        println("some future starts")
+        sleep(sec = 0.2)
+        println("some future ends")
+        3
+      } onSuccess {
+        case i: Int => println(s"declarative style: $i")
+      }
+      sleep(sec = 1.5)
+    }
+
+    demoBlock("errors in future") {
       val errorFuture: Future[Int] = Future {
-        throw new MyError("bubu")
+        throw new MyError("zooo")
       }
 
-      val unknownErrorFuture: Future[Int] = Future {
-        throw new MyError2("bubu")
-      }
+      errorFuture onSuccess universalCallbackBuilder("errorFuture") //never calls
+      errorFuture onFailure myErrorCallbackBuilder("myError #1")
+      errorFuture onFailure myErrorCallbackBuilder("myError #2")
+      errorFuture onFailure myErrorCallbackBuilder("myError #3") //callback order not garanted
 
-      errorFuture onSuccess universalCallbackBuilder("errorFuture")
-      errorFuture onFailure universalCallbackBuilder("errorFuture onFailure")
       errorFuture onFailure universalCallbackBuilder("errorFuture onFailure2")
 
-      unknownErrorFuture onSuccess universalCallbackBuilder("unknownErrorFuture")
-      //unknownErrorFuture onFailure (universalCallback orElse alwaysMatchCallback)
+      sleep(sec = 4)
+
+      val unknownErrorFuture: Future[Int] = Future {
+        throw new MyError2("bubu 2")
+      }
+
       unknownErrorFuture onFailure universalCallbackBuilder("unknownErrorFuture onFailure")
+      unknownErrorFuture onFailure myErrorCallbackBuilder("unknownErrorFuture myErrorCallbackBuilder")
+      unknownErrorFuture onFailure {
+        myErrorCallbackBuilder("unknownErrorFuture myErrorCallbackBuilder") orElse alwaysMatchCallback
+      }
+
+      sleep(sec = 4)
     }
+
+    demoBlock("Orphan futures") {
+
+      val returnUnmatchedInt: Future[Int] = Future {
+        0
+      }
+      returnUnmatchedInt onSuccess universalCallbackBuilder()
+      sleep(sec = 2)
+
+      val returnErrorWithoutErrorCallback: Future[Int] = Future {
+        throw new MyError(":(")
+      }
+      returnErrorWithoutErrorCallback onSuccess universalCallbackBuilder()
+      sleep(sec = 2)
+
+      val returnUnmatchedError: Future[Int] = Future {
+        throw new MyError2(":((((")
+      }
+      returnUnmatchedError onFailure myErrorCallbackBuilder("hm...")
+      sleep(sec = 2)
+
+      val fatalError: Future[Int] = Future {
+        throw new RuntimeException("wtf?")
+      }
+      fatalError onFailure myErrorCallbackBuilder("hm...")
+
+      sleep(sec = 0.5)
+      println("oops, nothing fatal happened")
+      sleep(sec = 2)
+
+      println("now burn!")
+      Await.result(fatalError, 2 seconds)
+      println("end (never called)")
+    }
+
+    //TODO: create execution context and test catching some errors
 
   }
 }
