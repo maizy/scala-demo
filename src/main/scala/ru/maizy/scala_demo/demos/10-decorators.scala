9e6f8735016da45002394233ebb2639bfb68870e
Nikita Kovaliov <nikita@maizy.ru>
2015-05-27 19:15:34 +0300

    simple decorators

---
+++ src/main/scala/ru/maizy/scala_demo/demos/10-decorators.scala
@@ -0,0 +1,88 @@
+package ru.maizy.scala_demo.demos
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2013
+ * See LICENSE.txt for details.
+ */
+import ru.maizy.scala_demo.{Settings, Demo}
+import ru.maizy.scala_demo.demoBlock
+
+
+class DecoratorsDemo extends Demo {
+  val name: String = "decorators"
+
+  def run(settings: Settings) {
+    class Prefixer(implicit prefix: String)
+
+    object TestObj {
+      def simple(s: String): String = s"s: $s"
+      def describe(a: String, b: Int): String = s"a: $a, b: $b"
+    }
+
+    object TestObjWithImplicit {
+      def simpleWithPrefixer(s: String)(implicit p: Prefixer): String = s"[$p] s: $s"
+      def describeWithPrefixer(a: String, b: String)(implicit p: Prefixer): String
+      = s"[$p] a: $a, b: $b"
+    }
+
+    class SimpleDecorator[I](
+        val postfix: String,
+        val func: (I) => String,
+        val keyFunc: (I) => String  //to demonstrate other internal usage of params
+                                    //ex: computer cache key
+                              ) {
+      def apply(params: I): String =  {
+        val key = keyFunc(params)
+        s"[${func(params)}]=$postfix (call with key=$key)"
+      }
+    }
+
+    demoBlock("simple decorator for arity=1") {
+
+      val func: (String => String) = TestObj.simple //just to demonstrate type
+
+      val simpleDecorated = new SimpleDecorator[String](
+        postfix = "bar",
+        func = TestObj.simple,
+        keyFunc = s => {
+          val hash = s.hashCode.toString
+          s.hashCode.toString.substring(0, scala.math.min(10, hash.length))
+        }
+      )
+
+      println(s"Before decorate: ${TestObj.simple("foo")}")
+      println(s"After decorate: ${simpleDecorated("foo")}")
+
+      val secondLevelDecorator = new SimpleDecorator[String](
+        postfix = "second",
+        func = simpleDecorated.apply,
+        keyFunc = s => {
+          val hash = s.hashCode.toString
+          s.hashCode.toString.substring(scala.math.max(hash.length - 5, 0), hash.length)
+        }
+      )
+      println(s"After second decorator: ${secondLevelDecorator("foo")}")
+
+    }
+
+    demoBlock("simple decorator with arity>1") {
+      //types:
+      val func: (String, Int) => String = TestObj.describe
+
+      val funcTupled: (Tuple2[String, Int] => String) = func.tupled
+      // or with syntax shugar, lisp-style :)
+      val funcTupledShugar: (((String, Int)) => String) = func.tupled
+
+      val multiArityDecorator = new SimpleDecorator[(String, Int)](
+        func = (TestObj.describe _).tupled, // use _ to convert method to anon func
+        postfix = "baz",
+        keyFunc = {
+          case (a, b) => s"$a-$b"
+        }
+      )
+
+      println(s"Before decorate: ${TestObj.describe("bebe", 10)}")
+      println(s"After decorate: ${multiArityDecorator("bebe", 10)}")
+    }
+
+  }
+}
